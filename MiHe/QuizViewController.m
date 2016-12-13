3ec53c308294e3945baff621ab16772566b91b07
Michael Yu <michaely@sbmelmac-05560.local>
2016-12-13 15:13:49 +1100

    init working version

--- MiHe/QuizViewController.m
+++ MiHe/QuizViewController.m
@@ -10,13 +10,60 @@
 
 @interface QuizViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+
+@property (nonatomic, copy) NSArray *answers;
+
 @property (nonatomic, weak) IBOutlet UILabel *questionLabel;
 
 @property (nonatomic, weak) IBOutlet UILabel *answerLabel;
 
+
 @end
 
 @implementation QuizViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+
+    if (self) {
+        self.questions = @[@"1+1?",
+                           @"2+2"];
+        
+        self.answers = @[@"2",
+                         @"4"];
+    }
+    
+    return self;
+}
+
+
+
+- (IBAction)showQuestion:(id)sender
+{
+    self.currentQuestionIndex++;
+    
+    if (self.currentQuestionIndex == [self.questions count]) {
+        self.currentQuestionIndex = 0;
+    }
+    
+    NSString *question = self.questions[self.currentQuestionIndex];
+    self.questionLabel.text = question;
+    
+    self.answerLabel.text = @"???";
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLabel.text = answer;
+}
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
